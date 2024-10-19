899a8fdc8812075994ffdd27c015050948386167
NuntichaThummeeSecE <n01687478@humber.ca>
2024-10-18 20:03:51 -0400

    try using api

--- lab8.js
+++ lab8.js
@@ -1,5 +1,11 @@
-const apiKey = RGAPI-f2ce3ef9-c397-4afd-906c-73ef4b1b2870;
 
-function getChampion(){
-    fetch(``)
+function getRandomCat() {
+    fetch(`https://api.thecatapi.com/v1/images/search`)
+    .then(response => response.json())
+    .then (data => {
+        console.log(data);
+        let catImg = data[0].url;
+        document.getElementById(`catImage`).src = catImg;
+    })
+    .catch(error => console.error(`Error fretching data: `, error));
 }
